2b0189c52f7b96424fb6308f15bae0ba0e8391f7
Nicolas Blaisdell <nblaisdell2@gmail.com>
2023-08-01 11:16:59 -0400

    fixed test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,3 @@ import App from "./routes/Home";
 import React from "react";
 import "@testing-library/jest-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
-
-describe("App Page Tests", () => {});
